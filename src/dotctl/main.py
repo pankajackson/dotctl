eec1b000d870db7a050c978d8b1deb3ae2c605d3
Pankaj Jackson <pankajackson@live.co.uk>
2025-04-04 02:00:36 +0530

    implemented dynamic props

--- src/dotctl/main.py
+++ src/dotctl/main.py
@@ -30,31 +30,9 @@ class Action(Enum):
 
 
 class DotCtl:
-    def __init__(
-        self,
-        action: Action,
-        git_url: str | None = None,
-        profile: str | None = None,
-        config: str | None = None,
-        env: str | None = None,
-        skip_sudo: bool = False,
-        password: str | None = None,
-        details: bool = False,
-        fetch: bool = False,
-        no_confirm: bool = False,
-        *args,
-        **kwargs,
-    ):
+    def __init__(self, action: Action, **kwargs):
         self.action = action
-        self.git_url = git_url
-        self.profile = profile
-        self.config = config
-        self.env = str(env) if env else None
-        self.skip_sudo = skip_sudo
-        self.password = password
-        self.details = details
-        self.fetch = fetch
-        self.no_confirm = no_confirm
+        self.k_args: dict = kwargs
 
     def run(self):
         """Run the appropriate action based on the provided command."""
@@ -71,103 +49,67 @@ class DotCtl:
         }
         action_methods.get(self.action, lambda: None)()
 
+    def _build_props(self, defaults, *keys):
+        """Utility function to build properties dictionary dynamically."""
+        props = {k: v for k in keys if (v := self.k_args.get(k, None)) is not None}
+        return replace(defaults, **props)
+
     def init_profile(self):
-        initializer_props_dict = {}
-
-        if self.git_url:
-            initializer_props_dict["git_url"] = self.git_url
-        if self.profile:
-            initializer_props_dict["profile"] = self.profile
-        if self.config:
-            initializer_props_dict["custom_config"] = Path(self.config)
-        if self.env:
-            initializer_props_dict["env"] = self.env
-
-        initializer_props = replace(initializer_default_props, **initializer_props_dict)
-
-        initialise(initializer_props)
+        """Initialize a new dotfiles profile."""
+        props = self._build_props(
+            initializer_default_props, "git_url", "profile", "config", "env"
+        )
+        initialise(props)
 
     def save_dots(self):
-        saver_props_dict = {}
-        if self.skip_sudo:
-            saver_props_dict["skip_sudo"] = self.skip_sudo
-        if self.password:
-            saver_props_dict["password"] = self.password
-        if self.profile:
-            saver_props_dict["profile"] = self.profile
-        saver_props = replace(saver_default_props, **saver_props_dict)
-        save(saver_props)
+        """Save current dotfiles."""
+        props = self._build_props(
+            saver_default_props, "skip_sudo", "password", "profile"
+        )
+        save(props)
 
     def apply_dots(self):
-        apply_props_dict = {}
-        if self.skip_sudo:
-            apply_props_dict["skip_sudo"] = self.skip_sudo
-        if self.password:
-            apply_props_dict["password"] = self.password
-        if self.profile:
-            apply_props_dict["profile"] = self.profile
-        apply_props = replace(activator_default_props, **apply_props_dict)
-        apply(apply_props)
+        """Apply a saved dotfiles profile."""
+        props = self._build_props(
+            activator_default_props, "skip_sudo", "password", "profile"
+        )
+        apply(props)
 
     def list_profiles(self):
-        lister_props_dict = {}
-        if self.details:
-            lister_props_dict["details"] = self.details
-        if self.fetch:
-            lister_props_dict["fetch"] = self.fetch
-        lister_props = replace(lister_default_props, **lister_props_dict)
-        get_profile_list(lister_props)
+        """List available dotfiles profiles."""
+        props = self._build_props(lister_default_props, "details", "fetch")
+        get_profile_list(props)
 
     def switch_profile(self):
-        switcher_props_dict = {}
-        if self.profile:
-            switcher_props_dict["profile"] = self.profile
-        if self.fetch:
-            switcher_props_dict["fetch"] = self.fetch
-        switcher_props = replace(switcher_default_props, **switcher_props_dict)
-        switch(switcher_props)
+        """Switch to a different dotfiles profile."""
+        props = self._build_props(switcher_default_props, "profile", "fetch")
+        switch(props)
 
     def create_profile(self):
-        creator_props_dict = {}
-        if self.profile:
-            creator_props_dict["profile"] = self.profile
-        if self.fetch:
-            creator_props_dict["fetch"] = self.fetch
-        creator_props = replace(creator_default_props, **creator_props_dict)
-        create(creator_props)
+        """Create a new dotfiles profile."""
+        props = self._build_props(creator_default_props, "profile", "fetch")
+        create(props)
 
     def remove_profile(self):
-        remover_props_dict = {}
-        if self.profile:
-            remover_props_dict["profile"] = self.profile
-        if self.fetch:
-            remover_props_dict["fetch"] = self.fetch
-        if self.no_confirm:
-            remover_props_dict["no_confirm"] = self.no_confirm
-        remove_props = replace(remover_default_props, **remover_props_dict)
-        remove(remove_props)
+        """Remove an existing dotfiles profile."""
+        props = self._build_props(
+            remover_default_props, "profile", "fetch", "no_confirm"
+        )
+        remove(props)
 
     def export_profile(self):
-        exporter_props_dict = {}
-        if self.skip_sudo:
-            exporter_props_dict["skip_sudo"] = self.skip_sudo
-        if self.password:
-            exporter_props_dict["password"] = self.password
-        if self.profile:
-            exporter_props_dict["profile"] = self.profile
-        exporter_props = replace(exporter_default_props, **exporter_props_dict)
-        exporter(exporter_props)
+        """Export dotfiles profile."""
+        props = self._build_props(
+            exporter_default_props, "skip_sudo", "password", "profile"
+        )
+        exporter(props)
 
     def import_profile(self):
-        importer_props_dict = {}
-        if self.skip_sudo:
-            importer_props_dict["skip_sudo"] = self.skip_sudo
-        if self.password:
-            importer_props_dict["password"] = self.password
-        if self.profile:
-            importer_props_dict["profile"] = Path(self.profile)
-        importer_props = replace(importer_default_props, **importer_props_dict)
-        importer(importer_props)
+        """Import a dotfiles profile."""
+        props = self._build_props(
+            importer_default_props, "skip_sudo", "password", "profile"
+        )
+        importer(props)
 
 
 @exception_handler
